31d8f7c5cde8200f08069fa39b73806132e435ea
zuo <lipingtso@163.com>
2021-11-05 16:31:25 +0800

    初始化 eureka-server

---
+++ eureka-server/src/main/java/com/zuo/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.zuo.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+//声明为eureka的服务注册中心
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
---
+++ eureka-server/src/test/java/com/zuo/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.zuo.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("nihao");
+    }
+
+}
